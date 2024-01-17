842b6be439100c670dc949448eb88e7a04cb532b
cuber-dev <adepunandhu9@gmail.com>
2024-01-17 09:06:36 +0530

    contact-not-links-update

--- src/routes/components/Contact.jsx
+++ src/routes/components/Contact.jsx
@@ -1,8 +1,61 @@
 
 
 function Contact() {
+    const labelLinks = [
+        {
+            label : 'Product',
+            link : '/'
+        },
+        {
+            label : 'Features',
+            link : '/features'
+        },
+        {
+            label : 'MAX',
+            link : '/'
+        },
+        {
+            label : 'About',
+            link : '/about'
+        },
+        {
+            label : 'Support',
+            link : '/'
+        },
+    ]
+    const iconLinks = [
+        {
+            class : 'fa-brands fa-github',
+            link : '/'
+        },
+        {
+            class : 'fa-brands fa-twitter',
+            link : '/'
+        },
+        {
+            class : 'fa-brands fa-facebook',
+            link : '/'
+        },
+        {
+            class : 'fa-brands fa-instagram',
+            link : '/'
+        }
+    ]
     return ( <>
-        <h1>Contact</h1>
+        <section className="contact">
+            <ul className="label-links links">
+                {labelLinks.map((item,index) => (
+                    <li key={index} ><a href={item.link} className="link">{item.label}</a></li>
+                ))}
+            </ul>
+            <div className="divider"></div>
+            <ul className="icon-links links">
+                {iconLinks.map((item,index) => (
+                    <li key={index} ><a href={item.link} className="link"> <i className={item.class} ></i> </a></li>
+                ))}
+            </ul>
+            <p className="copy-right"> <i class="fa fa-copyright" aria-hidden="true"></i> All Rights Reserved 2024-2025.</p>
+        </section>
     </> );
 } 
 
