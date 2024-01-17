f6ca07ea3d2d8db5827d5775d039cc7b8f005f54
cuber-dev <adepunandhu9@gmail.com>
2024-01-17 20:12:03 +0530

    almost-done-update

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,12 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import LandingPage from "./components/LandingPage";
 import Results from "./components/Results";
 
 
 function Home() {
-    const [results,setResults] = useState([])
+    const baseUrl = 'https://a6864657-ac0b-4b3c-b208-31b1bf8eead8-00-3cry9l8we6yhg.pike.replit.dev';
+    const [results,setResults] = useState([]);
+
+    const update = async (url) => {
+        try{
+            const reqURL = `${baseUrl}/info?url=${url}`;
+            const res = await fetch(reqURL);
+            const data = await res.json();
+            console.log(data);
+            setResults( prev => [...prev,data]); 
+        }catch (e){
+            console.log(e);
+            setResults( prev => [...prev,e]); 
+        }
+    }
     return ( <>
-        <LandingPage />
+        <LandingPage update={update} />
         <Results results={results} />
     </>);
 }
--- src/routes/components/LandingPage.jsx
+++ src/routes/components/LandingPage.jsx
@@ -1,17 +1,29 @@
+import { useRef, useState } from "react";
 
-
-
-function LandingPage({}) {
+function LandingPage({ update }) {
+    const urlInputRef = useRef(null);
+    const [urlInput,setUrlInput] = useState('');
+    const handleSubmit = e => {
+        e.preventDefault();
+        if(urlInputRef.current  && urlInput.trim() !== ""){
+            update(urlInput);
+            setUrlInput(''); 
+        } 
+    }
     return ( <>
         <section className="landing-page">
             <h1 className="title" >Download YouTube Videos With <span className="accent" >MAX</span></h1>
-            <form className="url-form">
+            <form className="url-form" onSubmit={handleSubmit} >
                 <input 
-                    type="text" 
+                    type="text"  
                     name="url" 
                     placeholder="Paste Youtube Video URL Here!" 
                     id="url" 
                     className="url-input"
+                    required
+                    ref={urlInputRef}
+                    value={urlInput}
+                    onChange={e => setUrlInput(e.target.value)}
                 />
                 <button type="submit" className="url-download-btn">
                     <i className="fa-solid fa-paper-plane"></i>
